f38cf781f41a9e0d84f3698835cc56f1972c3083
913-Diaconu-Ana <daie2909@scs.ubbcluj.ro>
2022-04-18 10:16:46 +0300

    180422 seed data in various entities
    university year, student university year, semester, course enrolment, course

--- Data/Program.cs
+++ Data/Program.cs
@@ -14,9 +14,346 @@ namespace Data
             
             dbContext.Database.Migrate();
 
-            SeedUsers(dbContext);
-            SeedSpecialization(dbContext);
             SeedGroup(dbContext);
+            SeedSpecialization(dbContext);
+            SeedUsers(dbContext);
+            seedUniversityYear (dbContext);
+            seedStudentUniversityYear(dbContext);
+            seedSemester(dbContext);
+            seedCourses(dbContext);
+            seedCourseEnrolments(dbContext);
+
+        }
+
+        public static void seedCourseEnrolments(UMSDatabaseContext context)
+        {
+            var enrolment = new CourseEnrolments()
+            {
+                StudentUniversityYearID = 1,
+                CoursesID = 6
+            };
+
+            if(context.CoursesEnrolments.FirstOrDefault(x => x.Id.Equals(1)) is null)
+            {
+                context.CoursesEnrolments.Add(enrolment);
+            }
+
+            enrolment = new CourseEnrolments()
+            {
+                StudentUniversityYearID = 1,
+                CoursesID = 5
+            };
+
+            if (context.CoursesEnrolments.FirstOrDefault(x => x.Id.Equals(2)) is null)
+            {
+                context.CoursesEnrolments.Add(enrolment);
+            }
+
+            enrolment = new CourseEnrolments()
+            {
+                StudentUniversityYearID = 2,
+                CoursesID = 1
+            };
+
+            if (context.CoursesEnrolments.FirstOrDefault(x => x.Id.Equals(3)) is null)
+            {
+                context.CoursesEnrolments.Add(enrolment);
+            }
+
+            enrolment = new CourseEnrolments()
+            {
+                StudentUniversityYearID = 2,
+                CoursesID = 2
+            };
+
+            if (context.CoursesEnrolments.FirstOrDefault(x => x.Id.Equals(4)) is null)
+            {
+                context.CoursesEnrolments.Add(enrolment);
+            }
+
+            enrolment = new CourseEnrolments()
+            {
+                StudentUniversityYearID = 2,
+                CoursesID = 3
+            };
+
+            if (context.CoursesEnrolments.FirstOrDefault(x => x.Id.Equals(5)) is null)
+            {
+                context.CoursesEnrolments.Add(enrolment);
+            }
+
+            enrolment = new CourseEnrolments()
+            {
+                StudentUniversityYearID = 3,
+                CoursesID = 5
+            };
+
+            if (context.CoursesEnrolments.FirstOrDefault(x => x.Id.Equals(6)) is null)
+            {
+                context.CoursesEnrolments.Add(enrolment);
+            }
+
+            enrolment = new CourseEnrolments()
+            {
+                StudentUniversityYearID = 3,
+                CoursesID = 6
+            };
+
+            if (context.CoursesEnrolments.FirstOrDefault(x => x.Id.Equals(7)) is null)
+            {
+                context.CoursesEnrolments.Add(enrolment);
+            }
+
+            context.SaveChanges();
+        }
+
+        public static void seedCourses(UMSDatabaseContext context)
+        {
+            var course = new Courses()
+            {
+                DisciplineName = "Algebră",
+                TeacherId = 1,
+                SemesterId = 1,
+                OptionalFlag = false,
+                NumberOfStudents = 200,
+                SpecializationID = 2,
+
+            };
+
+            if (context.Courses.FirstOrDefault(x => x.Id.Equals(1)) is null)
+            {
+                context.Courses.Add(course);
+            }
+
+            course = new Courses()
+            {
+                DisciplineName = "Analiză matematică",
+                TeacherId = 1,
+                SemesterId = 1,
+                OptionalFlag = false,
+                NumberOfStudents = 200,
+                SpecializationID = 2,
+
+            };
+
+            if (context.Courses.FirstOrDefault(x => x.Id.Equals(2)) is null)
+            {
+                context.Courses.Add(course);
+            }
+
+            course = new Courses()
+            {
+                DisciplineName = "Sisteme de operare",
+                TeacherId = 1,
+                SemesterId = 2,
+                OptionalFlag = false,
+                NumberOfStudents = 200,
+                SpecializationID = 2,
+
+            };
+
+            if (context.Courses.FirstOrDefault(x => x.Id.Equals(3)) is null)
+            {
+                context.Courses.Add(course);
+            }
+
+            course = new Courses()
+            {
+                DisciplineName = "OOP",
+                TeacherId = 1,
+                SemesterId = 2,
+                OptionalFlag = false,
+                NumberOfStudents = 200,
+                SpecializationID = 2,
+
+            };
+
+            if (context.Courses.FirstOrDefault(x => x.Id.Equals(4)) is null)
+            {
+                context.Courses.Add(course);
+            }
+
+            course = new Courses()
+            {
+                DisciplineName = "Rețele de calculatoare",
+                TeacherId = 2,
+                SemesterId = 1,
+                OptionalFlag = false,
+                NumberOfStudents = 150,
+                SpecializationID = 1,
+
+            };
+
+            if (context.Courses.FirstOrDefault(x => x.Id.Equals(5)) is null)
+            {
+                context.Courses.Add(course);
+            }
+
+            course = new Courses()
+            {
+                DisciplineName = "Baze de date",
+                TeacherId = 2,
+                SemesterId = 1,
+                OptionalFlag = false,
+                NumberOfStudents = 200,
+                SpecializationID = 1,
+
+            };
+
+            if (context.Courses.FirstOrDefault(x => x.Id.Equals(6)) is null)
+            {
+                context.Courses.Add(course);
+            }
+
+            context.SaveChanges();  
+        }
+
+        public static void seedSemester(UMSDatabaseContext context)
+        {
+            var sem = new Semester()
+            {
+                UniversityYearID = 1,
+                SemesterDetails = 1
+            };
+
+            if(context.Semesters.FirstOrDefault(x=> x.UniversityYearID.Equals(1) && x.SemesterDetails.Equals(1)) is null)
+            {
+                context.Semesters.Add(sem);
+            }
+
+            sem = new Semester()
+            {
+                UniversityYearID = 1,
+                SemesterDetails = 2
+            };
+
+            if (context.Semesters.FirstOrDefault(x => x.UniversityYearID.Equals(1) && x.SemesterDetails.Equals(2)) is null)
+            {
+                context.Semesters.Add(sem);
+            }
+
+            sem = new Semester()
+            {
+                UniversityYearID = 2,
+                SemesterDetails = 1
+            };
+
+            if (context.Semesters.FirstOrDefault(x => x.UniversityYearID.Equals(2) && x.SemesterDetails.Equals(1)) is null)
+            {
+                context.Semesters.Add(sem);
+            }
+
+            sem = new Semester()
+            {
+                UniversityYearID = 2,
+                SemesterDetails = 2
+            };
+
+            if (context.Semesters.FirstOrDefault(x => x.UniversityYearID.Equals(2) && x.SemesterDetails.Equals(2)) is null)
+            {
+                context.Semesters.Add(sem);
+            }
+
+            sem = new Semester()
+            {
+                UniversityYearID = 3,
+                SemesterDetails = 1
+            };
+
+            if (context.Semesters.FirstOrDefault(x => x.UniversityYearID.Equals(3) && x.SemesterDetails.Equals(1)) is null)
+            {
+                context.Semesters.Add(sem);
+            }
+
+            sem = new Semester()
+            {
+                UniversityYearID = 3,
+                SemesterDetails = 2
+            };
+
+            if (context.Semesters.FirstOrDefault(x => x.UniversityYearID.Equals(3) && x.SemesterDetails.Equals(2)) is null)
+            {
+                context.Semesters.Add(sem);
+            }
+
+            context.SaveChanges();
+        }
+
+        public static void seedStudentUniversityYear(UMSDatabaseContext context)
+        {
+            var contract = new StudentUniversityYear()
+            {
+                StudentId = 1,
+                UniversityYearId = 1
+            };
+
+            if (context.StudentUniversityYears.FirstOrDefault(x => x.StudentId.Equals(3) && x.UniversityYearId.Equals(1)) is null)
+            {
+                context.StudentUniversityYears.Add(contract);
+            }
+
+            contract = new StudentUniversityYear()
+            {
+                StudentId = 2,
+                UniversityYearId = 2
+            };
+
+            if (context.StudentUniversityYears.FirstOrDefault(x => x.StudentId.Equals(3) && x.UniversityYearId.Equals(1)) is null)
+            {
+                context.StudentUniversityYears.Add(contract);
+            }
+
+            contract = new StudentUniversityYear()
+            {
+                StudentId = 3,
+                UniversityYearId = 3
+            };
+
+            if (context.StudentUniversityYears.FirstOrDefault(x => x.StudentId.Equals(4) && x.UniversityYearId.Equals(3)) is null)
+            {
+                context.StudentUniversityYears.Add(contract);
+            }
+
+
+
+            context.SaveChanges();
+
+        }
+
+        public static void seedUniversityYear(UMSDatabaseContext context)
+        {
+            var year1 = new UniversityYear()
+            {
+                Year = 1
+            };
+
+            if(context.UniversityYears.FirstOrDefault(x => x.Year.Equals(1)) is null)
+            {
+                context.UniversityYears.Add(year1);
+            }
+
+            var year2 = new UniversityYear()
+            {
+                Year = 2
+            };
+
+            if (context.UniversityYears.FirstOrDefault(x => x.Year.Equals(2)) is null)
+            {
+                context.UniversityYears.Add(year2);
+            }
+
+            var year3 = new UniversityYear()
+            {
+                Year = 3
+            };
+
+            if (context.UniversityYears.FirstOrDefault(x => x.Year.Equals(3)) is null)
+            {
+                context.UniversityYears.Add(year3);
+            }
+
+            context.SaveChanges();
+
         }
 
         public static void SeedUsers(UMSDatabaseContext context)
@@ -167,6 +504,31 @@ namespace Data
             if (context.Users.FirstOrDefault(x => x.Username.Equals("student2")) is null)
             {
                 context.Users.Add(student2);
+            }
+
+            student = new User()
+            {
+                Role = Roles.Student,
+                Username = "student3",
+                Password = "password7", // Bcrypt.hash("pasword")
+                UserProfile = new UserProfile()
+                {
+                    Age = 20,
+                    Email = "student7@test.com",
+                    Fullname = "Student7 fullname",
+                    ProfileImageUrl =
+                        "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/2020-Chevrolet-Corvette-Stingray/0x0.jpg?fit=crop&format=jpg&crop=4560,2565,x790,y784,safe"
+                },
+                Student = new Student()
+                {
+                    GroupId = 2,
+                    SpecializationId = 2
+                }
+            };
+
+            if (context.Users.FirstOrDefault(x => x.Username.Equals("student3")) is null)
+            {
+                context.Users.Add(student);
             }
 
             // add teacher, add adminStaff
