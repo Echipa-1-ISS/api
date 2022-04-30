64be48e831cc6067058bd6418a2a0702a5239e8e
913-Dobra-Andrei <dobra_andrei01@yahoo.com>
2022-04-30 21:57:56 +0300

    created Courses controller + service

---
+++ Api/Controllers/CoursesController.cs
@@ -0,0 +1,29 @@
+﻿using Business.Services;
+using Data;
+using Data.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Api.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class CoursesController : ControllerBase {
+    private UMSDatabaseContext _context;
+    private CoursesService _service;
+
+    public CoursesController(UMSDatabaseContext context, CoursesService service) {
+        _context = context;
+        _service = service;
+    }
+
+    [HttpGet()]
+    [AllowAnonymous]
+    public List<Courses> GetCourses() {
+        return _context.Courses.ToList();
+    }
+        
+}
