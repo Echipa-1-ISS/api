5ad167b37064fec402f5ce4406a10d60f22aa387
913-Dobra-Andrei <dobra_andrei01@yahoo.com>
2022-05-07 23:47:49 +0300

    added Get for all courses + getSemestersAndSpecializations

--- Business/Services/CoursesService.cs
+++ Business/Services/CoursesService.cs
@@ -1,4 +1,5 @@
-﻿using Data;
+﻿using Business.DTOs;
+using Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +12,15 @@ public class CoursesService {
 
     public CoursesService(UMSDatabaseContext context) {
         _context = context;
+    }
+
+    public SemestersAndSpecializationsResponse GetSemestersAndSpecializations() {
+        SemestersAndSpecializationsResponse SemestersAndSpecializations = new SemestersAndSpecializationsResponse();
+        SemestersAndSpecializations.Semesters = new List<SemesterDTO>();
+        SemestersAndSpecializations.Specializations = new List<SpecializationDTO>();
+        _context.Specializations.ToList().ForEach(s => SemestersAndSpecializations.Specializations.Add(new SpecializationDTO { Id = s.Id, Name = s.Name }));
+        _context.Semesters.ToList().ForEach(s => SemestersAndSpecializations.Semesters.Add(new SemesterDTO { Id = s.Id, SemesterDetails = s.SemesterDetails, UniversityYearID = s.UniversityYearID }));
+
+        return SemestersAndSpecializations;
     }
 }
