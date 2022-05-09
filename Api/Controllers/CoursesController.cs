2f27ea4b6fbe00a3bd103102c7b3cead4263ebd0
AmaliaDuma <amalia.diana01@gmail.com>
2022-05-09 15:53:54 +0300

    AddOptionalCourse
    New method in courses controller which finds the associate teacherId and checks how many optional courses he has. If <2 he can add more .
    AddOptionalCourseRequest class

--- Api/Controllers/CoursesController.cs
+++ Api/Controllers/CoursesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
+using Api.Requests;
 
 namespace Api.Controllers;
 
@@ -34,5 +35,32 @@ public class CoursesController : ControllerBase {
         return _service.GetSemestersAndSpecializations();
     }
 
+    [AllowAnonymous]
+    [HttpPost()]
+    public int AddOptionalCourse(AddOptionalCourseRequest request)
+    {
+        var teacherId = _context.Teachers.Where(o => o.UserId == request.UserId).Select(o => o.Id).SingleOrDefault();
+        var noOptionalCourses = _context.Courses.Where(o => (o.TeacherId == teacherId) && (o.OptionalFlag.Equals(true)))
+            .Count();
+
+        if (noOptionalCourses == 2) return 0;
+        else
+        {
+            var optionalCourse = new Courses
+            {
+                DisciplineName = request.Name,
+                TeacherId = teacherId,
+                SemesterId = request.SemesterId,
+                OptionalFlag = true,
+                NumberOfStudents = 0,
+                SpecializationID = request.SpecializationId
+            };
+            _context.Courses.Add(optionalCourse);
+            _context.SaveChanges();
+
+            noOptionalCourses++;
+            return 2 - noOptionalCourses;
+        }
+    }
 
 }
